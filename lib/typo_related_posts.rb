ea470eadc3e8693cbd65d90567e0172c8edd0465
Harry Seldon <harry.seldon.f@gmail.com>
2009-01-11 22:28:07 +0100

    correct bug with permalinks

--- lib/typo_related_posts.rb
+++ lib/typo_related_posts.rb
@@ -2,20 +2,23 @@
 module Typorelatedposts
   def display_related_posts
     if !@article.tags.empty?
-    	mylist = Array.new
-    	@article.tags.each { |tag| 
-    		mylist += Tag.find_by_name(tag.name).articles
-    	}
+      mylist = Array.new
+      @article.tags.each { |tag| 
+        mylist += Tag.find_by_name(tag.name).articles
+      }
     mylist.uniq
     tablo = mylist.sort_by {rand}[0,5]
 
     html = '<div id="related">'
-    html += '<h3>À lire également</h3>'
-    html += '<p>Si cet article vous a intéressé, vous pouvez poursuivre votre lecture avec ces billets similaires :</p>'	
+#    html += '<h3>À lire également</h3>'
+    html += '<h3>Read also</h3>'
+#    html += '<p>Si cet article vous a intéressé, vous pouvez poursuivre votre lecture avec ces billets similaires :</p>' 
+    html += '<p>If this article has interested you, we suggest you read these related posts:</p>'
     html += '<ul>'
     tablo.each { |article|
-
-    html += "<li><a href='#{this_blog.base_url}#{article.permalink}'>#{article.title}</a></li>"
+    
+#    html += "<li><a href='#{this_blog.base_url}#{article.permalink}'>#{article.title}</a></li>"
+    html += "<li><a href='#{article.permalink_url}'>#{article.title}</a></li>"
     }
     html += '</ul>'
     html += '</div>'
